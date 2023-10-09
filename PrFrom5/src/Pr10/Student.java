a5d48070610a1b055f698b30edd0ffb2332f0d0e
Dmitriy <puls_2@mail.ru>
2023-10-09 21:29:30 +0300

    FromPr5

---
+++ PrFrom5/src/Pr10/Student.java
@@ -0,0 +1,70 @@
+package Pr10;
+
+public class Student { //Имя, Фамилия, Специальность, Курс, Группа
+    private String name;
+    private String surname;
+    private String speciality;
+    private int course;
+    private String group;
+    private Double GPA;
+
+    public Student(String name, String surname, String speciality, int course, String group, double GPA) {
+        this.name = name;
+        this.surname = surname;
+        this.speciality = speciality;
+        this.course = course;
+        this.group = group;
+        this.GPA = GPA;
+    }
+
+    public Student() {
+    }
+
+    public Double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(Double GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
